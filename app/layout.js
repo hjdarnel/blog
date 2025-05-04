e6a13318384157490ca3d57c164405bf848ce88e
Henry Darnell <hjdarnel@users.noreply.github.com>
2025-05-04 02:32:40 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import PlausibleProvider from "next-plausible";
 import Link from "./Link";
 import HomeLink from "./HomeLink";
 import { serif } from "./fonts";
+import UmamiAnalytics from "./UmamiAnalytics";
 import "./global.css";
 
 export const metadata = {
@@ -12,22 +12,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={serif.className}>
       <body className="mx-auto max-w-2xl bg-[--bg] px-5 py-12 text-[--text]">
-        <PlausibleProvider domain="darnell.io">
-          <header className="mb-12 flex flex-row place-content-between">
-            <HomeLink />
-            <span className="relative top-[4px] italic">
-              by{" "}
-              <Link href="https://darnell.io" target="_blank">
-                <img
-                  alt="Henry Darnell"
-                  src="https://github.com/hjdarnel.png"
-                  className="relative -top-1 mx-1 inline h-8 w-8 rounded-full"
-                />
-              </Link>
-            </span>
-          </header>
-          <main>{children}</main>
-        </PlausibleProvider>
+        <UmamiAnalytics />
+        <header className="mb-12 flex flex-row place-content-between">
+          <HomeLink />
+          <span className="relative top-[4px] italic">
+            by{" "}
+            <Link href="https://darnell.io" target="_blank">
+              <img
+                alt="Henry Darnell"
+                src="https://github.com/hjdarnel.png"
+                className="relative -top-1 mx-1 inline h-8 w-8 rounded-full"
+              />
+            </Link>
+          </span>
+        </header>
+        <main>{children}</main>
       </body>
     </html>
   );
